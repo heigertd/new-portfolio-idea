7e8d7b562785781b982b944dc0c9a9170adf0500
Devin Heigert <heigertd@gmail.com>
2020-11-04 17:05:54 -0800

    added more reveal text

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,6 +4,8 @@ import data from '../data.js';
 import Experience from '../Components/Experience/Experience'
 import './Main.css';
 
+//TODO: Add svelte to experience
+
 export default function Main() {
     const [bigState, setBigState] = useState([]);
 
@@ -30,7 +32,7 @@ export default function Main() {
                     <div className={bigState === 'box1'? 'hidden' : ''}>
                         <button value = 'box1' onClick = {big}>About</button>
                         <div className = 'hidden-text'>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <h3>Seattle-based Web Developer. Click to find out more.</h3>
                         </div>
                     </div>
                     <div className={bigState === 'box1'? '' : 'hidden'}>
@@ -57,7 +59,17 @@ export default function Main() {
                 </div>
                 <div className = {bigState === 'box2'? 'box2 expanded' : 'box2 shrink'}>
                     <div className={bigState === 'box2'? 'hidden' : ''}>
-                        <button value = 'box2' onClick = {big}>Expand</button>
+                        <button value = 'box2' onClick = {big}>Portfolio</button>
+                        <div className = 'hidden-text'>
+                            <h3>Portfolio reveal text: Experience in</h3>
+                            <div className = 'xp-div'>
+                                {data[0].xp.map(element => { 
+                                    return <Experience image = {element.image} title = {element.title} />
+                                })} 
+                            </div>
+                            <h3>Click to view some projects</h3>
+                            
+                        </div>
                     </div>
                     <div className={bigState === 'box2'? '' : 'hidden'}>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -66,7 +78,10 @@ export default function Main() {
                 </div>
                 <div className = {bigState === 'box3'? 'box3 expanded' : 'box3 shrink'}>
                 <div className={bigState === 'box3'? 'hidden' : ''}>
-                        <button value = 'box3' onClick = {big}>Expand</button>
+                        <button value = 'box3' onClick = {big}>Contact</button>
+                        <div className = 'hidden-text'>
+                            <h3>Contact reveal text: Eager to get in contact? click here!</h3>
+                        </div>
                     </div>
                     <div className={bigState === 'box3'? '' : 'hidden'}>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
