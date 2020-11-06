8be48bb62b7b0f9743c49365c89cdd9543ddcaca
Devin Heigert <heigertd@gmail.com>
2020-11-06 14:21:11 -0800

    fixed portfolio layout and expand transition

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -10,13 +10,12 @@ import Project from '../Components/Project/Project'
 import './Main.css';
 
 //TODO: Add svelte to experience
-//      fix portfolio layout
 
 export default function Main() {
     const [bigState, setBigState] = useState([]);
 
     useEffect(() => {
-        setBigState('box2');
+        setBigState('');
     }, [])
 
     function big(e){
@@ -37,9 +36,6 @@ export default function Main() {
                 <div className = {bigState === 'box1'? 'box1 expanded' : 'box1 shrink'}>
                     <div className={bigState === 'box1'? 'hidden' : ''}>
                         <button value = 'box1' onClick = {big}>About</button>
-                        <div className = 'hidden-text'>
-                            <h3>Seattle-based Web Developer. Click to find out more.</h3>
-                        </div>
                     </div>
                     <div className={bigState === 'box1'? '' : 'hidden'}>
                         <div className = 'button-div'>
@@ -66,18 +62,8 @@ export default function Main() {
                 <div className = {bigState === 'box2'? 'box2 expanded' : 'box2 shrink'}>
                     <div className={bigState === 'box2'? 'hidden' : ''}>
                         <button value = 'box2' onClick = {big}>Portfolio</button>
-                        <div className = 'hidden-text'>
-                            <h3>Portfolio reveal text: Experience in</h3>
-                            <div className = 'xp-div'>
-                                {data[0].xp.map(element => { 
-                                    return <Experience image = {element.image} title = {element.title} />
-                                })} 
-                            </div>
-                            <h3>Click to view some projects</h3>
-                            
-                        </div>
                     </div>
-                    <div className={bigState === 'box2'? '' : 'hidden'}>
+                    <div className={bigState === 'box2'? 'portfolio' : 'hidden'}>
                         <div className = 'button-div'>
                             <button onClick = {small}>X</button>
                         </div>
@@ -92,9 +78,6 @@ export default function Main() {
                 <div className = {bigState === 'box3'? 'box3 expanded' : 'box3 shrink'}>
                 <div className={bigState === 'box3'? 'hidden' : ''}>
                         <button value = 'box3' onClick = {big}>Contact</button>
-                        <div className = 'hidden-text'>
-                            <h3>Contact reveal text: Eager to get in contact? click here!</h3>
-                        </div>
                     </div>
                     <div className={bigState === 'box3'? '' : 'hidden'}>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
